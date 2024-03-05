45f1df96bfdbd1efa5dc0bd0f880e70f1cddc065
Dripa Alexandr <forsyteRun@gmail.com>
2024-03-05 18:18:45 +0200

    feat: init sequalize

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
-
+import { SequelizeModule } from '@nestjs/sequelize';
 @Module({
-  imports: [UsersModule],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'root',
+      password: 'root',
+      database: 'nest-basic',
+      models: [],
+      autoLoadModels: true,
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
