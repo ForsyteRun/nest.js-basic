da9ef1fe123e760a70bf1aca4ad9ca31d64295dc
Dripa Alexandr <forsyteRun@gmail.com>
2024-03-06 14:40:58 +0200

    feat: cross-env capability

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://forsyterun:C6oTJ8IchNbIE65D@jewerly.eum5syn.mongodb.net/jewerly?retryWrites=true&w=majority',
-    ),
+    ConfigModule.forRoot({ envFilePath: `.${process.env.NODE_ENV}.env` }),
+    MongooseModule.forRoot(process.env.MONGO_DB),
     UsersModule,
     ProductsModule,
   ],
