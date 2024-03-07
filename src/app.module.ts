dbadee1a5138494e3a3972d946349e0237ed0d32
Dripa Alexandr <forsyteRun@gmail.com>
2024-03-07 08:04:48 +0200

    feat: get string

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,7 @@
-import { UsersModule } from './users/users.module';
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://forsyterun:C6oTJ8IchNbIE65D@jewerly.eum5syn.mongodb.net/jewerly?retryWrites=true&w=majority',
-    ),
-    UsersModule,
-  ],
+  imports: [ProductsModule],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 5000;
 
+  const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
 
-  await app.listen(3000);
+  await app.listen(port, () => console.log(`servet run on ${port}`));
 }
+
 bootstrap();
