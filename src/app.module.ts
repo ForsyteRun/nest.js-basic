197f5502d6e124f8a12904ad6500333a8165c218
Dripa Alexandr <forsyteRun@gmail.com>
2024-03-11 11:33:12 +0200

    feat: template

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,7 @@
-import { UsersModule } from './users/users.module';
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://forsyterun:C6oTJ8IchNbIE65D@jewerly.eum5syn.mongodb.net/jewerly?retryWrites=true&w=majority',
-    ),
-    UsersModule,
-  ],
+  imports: [UserModule],
 })
 export class AppModule {}
