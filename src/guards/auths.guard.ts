c03d4c7f22b21b05562ca970d43bab239acca9d4
Dripa Alexandr <forsyteRun@gmail.com>
2024-03-11 12:13:32 +0200

    fix: type guard

--- src/guards/auths.guard.ts
+++ src/guards/auths.guard.ts
@@ -4,19 +4,16 @@ import {
   ExecutionContext,
   UnauthorizedException,
 } from '@nestjs/common';
-import { log } from 'console';
 import { Observable } from 'rxjs';
-
+import { Request } from 'express';
 @Injectable()
 export class AuthGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    const request = context.switchToHttp().getRequest();
+    const request = context.switchToHttp().getRequest<Request>();
 
     const token = request.headers;
-
-    console.log(request);
 
     if (token.authorization !== '1234') {
       throw new UnauthorizedException({ msg: 'wrong token' });
